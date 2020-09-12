569572c4600a7443b427a15981f2b436f6a5bb79
andrew <october072017@yandex.ru>
2020-09-12 13:19:45 +0600

    FB-001: Additional test.

--- src/test/java/io/andrewtxt/sudoku/component/SudokuResolverTest.java
+++ src/test/java/io/andrewtxt/sudoku/component/SudokuResolverTest.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 public class SudokuResolverTest {
 
     @Test
-    public void testSuccess() {
+    public void testSuccessSimple() {
         int[][] values = new int[][] {
                 {0, 1, 3, 8, 0, 0, 4, 0, 5},
                 {0, 2, 4, 6, 0, 5, 0, 0, 0},
@@ -18,6 +18,23 @@ public class SudokuResolverTest {
                 {0, 6, 9, 0, 0, 0, 7, 4, 0},
                 {0, 0, 0, 2, 0, 7, 6, 8, 0},
                 {1, 0, 2, 0, 0, 8, 3, 5, 0}
+        };
+        Table table = new SudokuResolver().resolve(values);
+        Assertions.assertThat(table.getCellStream().noneMatch(cell -> cell.getValue() == null));
+    }
+
+    @Test
+    public void testSuccessComplex() {
+        int[][] values = new int[][] {
+                {0, 0, 2, 0, 0, 0, 0, 4, 1},
+                {0, 0, 0, 0, 8, 2, 0, 7, 0},
+                {0, 0, 0, 0, 4, 0, 0, 0, 9},
+                {2, 0, 0, 0, 7, 9, 3, 0, 0},
+                {0, 1, 0, 0, 0, 0, 0, 8, 0},
+                {0, 0, 6, 8, 1, 0, 0, 0, 4},
+                {1, 0, 0, 0, 9, 0, 0, 0, 0},
+                {0, 6, 0, 4, 3, 0, 0, 0, 0},
+                {8, 5, 0, 0, 0, 0, 4, 0, 0}
         };
         Table table = new SudokuResolver().resolve(values);
         Assertions.assertThat(table.getCellStream().noneMatch(cell -> cell.getValue() == null));
